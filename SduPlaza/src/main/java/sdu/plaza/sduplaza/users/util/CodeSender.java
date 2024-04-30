40f033253d8b4c50f2718376b6b96985d07780bf
MmumD <mukhammedmarat04@gmail.com>
2024-04-30 14:58:57 +0500

    fixed sender

--- SduPlaza/src/main/java/sdu/plaza/sduplaza/users/util/CodeSender.java
+++ SduPlaza/src/main/java/sdu/plaza/sduplaza/users/util/CodeSender.java
@@ -4,23 +4,52 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
+
+import java.util.Properties;
 
 @Component
 @RequiredArgsConstructor
 public class CodeSender {
 
-    private final JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
     private String user;
 
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.host}")
+    private String host;
+
     public void sendCode(String toEmail, String code){
+        JavaMailSender mailSender = mailSender();
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setFrom(user);
         mailMessage.setTo(toEmail);
         mailMessage.setSubject("SDU PLAZA | Reset password");
         mailMessage.setText("Your code: " + code);
         mailSender.send(mailMessage);
+    }
+
+    private JavaMailSender mailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(587);
+        mailSender.setDefaultEncoding("UTF-8");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        return mailSender;
     }
 }
